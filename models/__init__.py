a1a45726dd116cefd4ecba5e54c836ee08c4f9d5
assaadturki <assaadturki@gmail.com>
2026-05-15 21:15:41 +0300

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -243,3 +243,42 @@ class Alimentation(db.Model):
     unite = db.Column(db.String(20))
     cout_tnd = db.Column(db.Numeric(10, 2))
     notes = db.Column(db.Text)
+
+class Proprietaire(db.Model):
+    __tablename__ = 'proprietaires'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100), nullable=False)
+    prenom = db.Column(db.String(100))
+    telephone = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    adresse = db.Column(db.Text)
+    notes = db.Column(db.Text)
+    date_creation = db.Column(db.DateTime, default=datetime.utcnow)
+    parts = db.relationship('PartProprietaire', backref='proprietaire', lazy=True, cascade='all, delete-orphan')
+    depenses_perso = db.relationship('DepenseProprietaire', backref='proprietaire', lazy=True, cascade='all, delete-orphan')
+
+
+class PartProprietaire(db.Model):
+    __tablename__ = 'parts_proprietaires'
+    id = db.Column(db.Integer, primary_key=True)
+    proprietaire_id = db.Column(db.Integer, db.ForeignKey('proprietaires.id'), nullable=False)
+    type_bien = db.Column(db.String(50), nullable=False)
+    bien_id = db.Column(db.Integer, nullable=False)
+    part_pct = db.Column(db.Float, nullable=False)
+    date_creation = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class DepenseProprietaire(db.Model):
+    __tablename__ = 'depenses_proprietaires'
+    id = db.Column(db.Integer, primary_key=True)
+    proprietaire_id = db.Column(db.Integer, db.ForeignKey('proprietaires.id'), nullable=False)
+    categorie = db.Column(db.String(100), nullable=False)
+    montant_tnd = db.Column(db.Numeric(10, 2), nullable=False)
+    date_depense = db.Column(db.Date, nullable=False)
+    description = db.Column(db.Text)
+    type_depense = db.Column(db.String(20), default='personnelle')
+    bien_type = db.Column(db.String(50))
+    bien_id = db.Column(db.Integer)
+    facture_fichier = db.Column(db.String(255))
+    notes = db.Column(db.Text)
+    date_creation = db.Column(db.DateTime, default=datetime.utcnow)
